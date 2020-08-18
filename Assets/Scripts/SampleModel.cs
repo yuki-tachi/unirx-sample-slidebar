bee03f86739fece4d23f2431ca6ddf8e76502842
Yuki Tachi <z.yuki.tachi@gree.net>
2020-08-18 17:38:43 +0900

    fix:基本ファイル追加、ver更新

---
+++ Assets/Scripts/SampleModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public enum PlayerState
+{
+    Stopped,
+    Playing
+}
+public class SampleModel : MonoBehaviour
+{
+    private PlayerState PlayerState;
+    private float PlaybackTime;
+    private bool IsLoop = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rp = new ReactiveProperty<int>(15);
+        rp.Value = 20;
+        var currentValue = rp.Value;
+        Debug.Log(currentValue);
+        rp.Subscribe(x => Debug.Log(x));
+
+        rp.Value = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SampleView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleView : MonoBehaviour
+{
+    [SerializeField] private Button PlayButton;
+    [SerializeField] private Button StopButton;
+    [SerializeField] private Toggle LoopToggle;
+    [SerializeField] private Slider SeekBar;
+    [SerializeField] private Text playbackTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
